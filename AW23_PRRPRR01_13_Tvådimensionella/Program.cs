a38c56a76f45a95df2a16822fdde8a24f39d227a
magisterblaman <hannes.blaman@lbs.se>
2024-11-11 13:56:19 +0100

    Additionstabell

--- AW23_PRRPRR01_13_Tvådimensionella/Program.cs
+++ AW23_PRRPRR01_13_Tvådimensionella/Program.cs
@@ -7,29 +7,32 @@ using System.Threading.Tasks;
 namespace AW23_PRRPRR01_13_Tvådimensionella {
 	internal class Program {
 		static void Main(string[] args) {
-			for (int j = 1; j <= 100; j++) {
-				for (int i = 0; i < j; i++) {
-					Console.Write("*");
+			for (int j = 0; j < 10; j++) {
+				for (int i = 0; i < 10; i++) {
+					Console.Write((i + j) + " ");
 				}
 				Console.WriteLine();
 			}
 
-			//for (int i = 0; i < 1; i++) {
-			//	Console.Write("*");
-			//}
-			//Console.WriteLine();
-			//for (int i = 0; i < 2; i++) {
-			//	Console.Write("*");
-			//}
-			//Console.WriteLine();
-			//for (int i = 0; i < 3; i++) {
-			//	Console.Write("*");
-			//}
-			//Console.WriteLine();
 			//for (int i = 0; i < 4; i++) {
-			//	Console.Write("*");
+			//	Console.Write((i + 0) + " ");
 			//}
-			Console.WriteLine();
+			//Console.WriteLine();
+
+			//for (int i = 0; i < 4; i++) {
+			//	Console.Write((i + 1) + " ");
+			//}
+			//Console.WriteLine();
+
+			//for (int i = 0; i < 4; i++) {
+			//	Console.Write((i + 2) + " ");
+			//}
+			//Console.WriteLine();
+
+			//for (int i = 0; i < 4; i++) {
+			//	Console.Write((i + 3) + " ");
+			//}
+			//Console.WriteLine();
 		}
 	}
 }
